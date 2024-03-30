67638b3445685577e3e93212e94ee58070f6789c
DiegoJDArias <97647686+DiegoJDArias@users.noreply.github.com>
2024-03-29 23:16:29 -0300

    Agregado

---
+++ Introducción/src/Ejemplo.java
@@ -0,0 +1,8 @@
+public class Ejemplo {
+	private int var1;
+	private int var2;
+
+	public int getVar1() {
+		return var1;
+	}
+}
---
+++ Introducción/src/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// Mostrar por consola "Hola Mundo"
+		System.out.println("Hola Mundo!!!");
+	}
+}
