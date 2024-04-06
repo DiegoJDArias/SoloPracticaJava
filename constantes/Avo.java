a5a784da694777461892dc64364986ba6b3d194e
DiegoJDArias <97647686+DiegoJDArias@users.noreply.github.com>
2024-04-06 20:32:08 -0300

    Agregado

---
+++ constantes/Avo.java
@@ -0,0 +1,5 @@
+package constantes;
+
+public class Avo {
+	final double AVOGADRO = 6.023e23;
+}
---
+++ constantes/VerificarAvogadro.java
@@ -0,0 +1,13 @@
+package constantes;
+
+public class VerificarAvogadro {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Avo a = new Avo();
+	//	a.AVOGADRO = 8;
+	}
+
+}
