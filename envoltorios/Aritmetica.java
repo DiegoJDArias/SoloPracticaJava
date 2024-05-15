ea8da8cbf18fcc4f931ac34a128d6ba80ea51f0d
DiegoJDArias <97647686+DiegoJDArias@users.noreply.github.com>
2024-05-15 19:45:17 -0300

    Agregado

---
+++ envoltorios/Aritmetica.java
@@ -0,0 +1,19 @@
+package envoltorios;
+
+public class Aritmetica {
+
+	/*
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Integer i = 6;
+		Integer j = new Integer(8);
+		int resultado;
+		Float f = new Float(4.3);
+		resultado = i + j + f.intValue();
+		
+		System.out.println("Resultado: " + resultado);
+
+	}
+
+}
